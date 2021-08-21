a2a1df0a6a9984117acf083f5238591e138d599b
Vlad Eliseev <v.eliseev@auraband.io>
2021-08-21 14:03:22 +0300

    Layout base elements on DashboardVC

---
+++ PomoDo/UI/ViewControllers/DashboardViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DashboardViewController.swift
+//  PomoDo
+//
+//  Created by Aura on 21.08.2021.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var newTaskField: UITextField!
+    @IBOutlet weak var tasksTableView: UITableView!
+    @IBOutlet weak var playNewTaskButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
